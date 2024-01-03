57b8f6dadd5e1e3148e3d7248a3cfe280b710fa7
Christine Matta <christine.onsi@gmail.com>
2024-01-03 22:21:35 +0100

    dealing with dynamic eventListners

--- react essentials/01-starting-project/src/App.jsx
+++ react essentials/01-starting-project/src/App.jsx
@@ -1,9 +1,17 @@
-import Header from "./components/Header.jsx"
+import { useState } from "react";
+import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+
 import { CORE_CONCEPTS } from "./data.js";
 
+import { EXAMPLES } from "./data.js";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState("components");
+  function selectHandler(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
   return (
     <div>
       <Header />
@@ -23,6 +31,24 @@ function App() {
               />
             ))}
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => selectHandler("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => selectHandler("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => selectHandler("props")}>Props</TabButton>
+            <TabButton onSelect={() => selectHandler("state")}>State</TabButton>
+          </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
